f956b1b4885849b0620f5d3f6c5137573df16025
huangxiaomiao <1037073278@qq.com>
2024-06-06 22:52:54 +0800

    合家云物业管理系统基本框架

---
+++ hejiayun/hjy-community/src/main/java/com/hxm/hjycommunity/common/core/domain/BaseEntity.java
@@ -0,0 +1,124 @@
+package com.hxm.hjycommunity.common.core.domain;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
+
+
+/**
+ * Entity 基类
+ * @author spikeCong
+ * @date 2023/2/23
+ **/
+public class BaseEntity implements Serializable {
+
+    /**
+     * alt+enter
+     */
+    private static final long serialVersionUID = -3627784373208054198L;
+
+    /**
+     * 搜索值
+     * @TableField(exist = false)注解加载bean属性上，表示当前属性不是数据库的字段，
+     * 但在项目中必须使用，这样在新增等使用bean的时候，mybatis-plus就会忽略这个，不会报错
+     */
+    @TableField(exist = false)
+    private String searchValue;
+
+    /**
+     * 创建者
+     * fill 在需要被填充的字段上使用注解，声明什么时候要被填充
+     * FieldFill.INSERT 只在插入时填充
+     * FieldFill.INSERT_UPDATE 插入和更新时都填充
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private String createBy;
+
+    /** 创建时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /** 更新者 */
+    @TableField(fill = FieldFill.INSERT)
+    private String updateBy;
+
+    /** 更新时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @TableField(fill = FieldFill.INSERT)
+    private Date updateTime;
+
+    /** 备注 */
+    private String remark;
+
+    /** 请求参数 */
+    @TableField(exist = false)
+    private Map<String, Object> params;
+
+    //get/set ......
+
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getSearchValue() {
+        return searchValue;
+    }
+
+    public void setSearchValue(String searchValue) {
+        this.searchValue = searchValue;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public Map<String, Object> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, Object> params) {
+        this.params = params;
+    }
+}
---
+++ hejiayun/hjy-community/src/test/java/com/hxm/hjycommunity/HjyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxm.hjycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjyCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
